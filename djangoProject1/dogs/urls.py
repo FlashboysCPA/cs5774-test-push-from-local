c5bcde9161ed6180e4130cadebe401a4a6b74838
jason <jason93@vt.edu>
2021-10-16 12:19:39 -0400

    project4

--- djangoProject1/dogs/urls.py
+++ djangoProject1/dogs/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-
 app_name = 'dogs'
-
 urlpatterns = [
 #     lostpost views
     path('', views.lostpost_list, name= 'lostpost_list'),
+    path('<int:story_id>', views.lostpost_story_detail, name='story-detail'),
+
 ]
